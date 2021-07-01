d5c718f18f299972a103c022eef001f0f1b5e8ec
Gaseines <83676306+Gaseines@users.noreply.github.com>
2021-07-01 19:15:12 -0300

    dale

---
+++ js/script.js
@@ -0,0 +1,142 @@
+
+var button = document.getElementById("b1")
+
+button.addEventListener("click", function () {
+
+    var p1 = document.getElementById("p1")
+
+    if (p2.style.display === "block") {
+        p2.style.display = "none"
+    }
+
+    if (p3.style.display === "block") {
+        p3.style.display = "none"
+    }
+
+    if (p5.style.display === "block") {
+        p5.style.display = "none"
+    }
+
+    if (p4.style.display === "block") {
+        p4.style.display = "none"
+    }
+
+    if (p1.style.display === "block") {
+        p1.style.display = "none";
+    } else {
+        p1.style.display = "block"
+    }
+})
+var button = document.getElementById("b2")
+
+button.addEventListener("click", function () {
+
+    var p2 = document.getElementById("p2")
+
+    if (p1.style.display === "block") {
+        p1.style.display = "none"
+    }
+
+    if (p3.style.display === "block") {
+        p3.style.display = "none"
+    }
+
+    if (p4.style.display === "block") {
+        p4.style.display = "none"
+    }
+
+    if (p5.style.display === "block") {
+        p5.style.display = "none"
+    }
+
+    if (p2.style.display === "block") {
+        p2.style.display = "none";
+    } else {
+        p2.style.display = "block"
+    }
+
+})
+var button = document.getElementById("b3")
+
+button.addEventListener("click", function () {
+
+    var p3 = document.getElementById("p3")
+
+    if (p2.style.display === "block") {
+        p2.style.display = "none"
+    }
+
+    if (p1.style.display === "block") {
+        p1.style.display = "none"
+    }
+
+    if (p4.style.display === "block") {
+        p4.style.display = "none"
+    }
+
+    if (p5.style.display === "block") {
+        p5.style.display = "none"
+    }
+
+    if (p3.style.display === "block") {
+        p3.style.display = "none";
+    } else {
+        p3.style.display = "block"
+    }
+})
+var button = document.getElementById("b4")
+
+button.addEventListener("click", function () {
+
+    var p4 = document.getElementById("p4")
+
+    if (p2.style.display === "block") {
+        p2.style.display = "none"
+    }
+
+    if (p1.style.display === "block") {
+        p1.style.display = "none"
+    }
+
+    if (p3.style.display === "block") {
+        p3.style.display = "none"
+    }
+
+    if (p5.style.display === "block") {
+        p5.style.display = "none"
+    }
+
+    if (p4.style.display === "block") {
+        p4.style.display = "none";
+    } else {
+        p4.style.display = "block"
+    }
+})
+var button = document.getElementById("b5")
+
+button.addEventListener("click", function () {
+
+    var p5 = document.getElementById("p5")
+
+    if (p2.style.display === "block") {
+        p2.style.display = "none"
+    }
+
+    if (p1.style.display === "block") {
+        p1.style.display = "none"
+    }
+
+    if (p3.style.display === "block") {
+        p3.style.display = "none"
+    }
+
+    if (p4.style.display === "block") {
+        p4.style.display = "none"
+    }
+
+    if (p5.style.display === "block") {
+        p5.style.display = "none";
+    } else {
+        p5.style.display = "block"
+    }
+})
